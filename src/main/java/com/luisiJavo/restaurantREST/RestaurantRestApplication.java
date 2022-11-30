6b113a4b6efe9ae670cf81e545c25eb75cda6635
luisiJavo <luis.aranda.lillo@gmail.com>
2022-11-30 12:15:37 +0100

    First commit

---
+++ src/main/java/com/luisiJavo/restaurantREST/RestaurantRestApplication.java
@@ -0,0 +1,13 @@
+package com.luisiJavo.restaurantREST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luisiJavo/restaurantREST/RestaurantRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luisiJavo.restaurantREST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
